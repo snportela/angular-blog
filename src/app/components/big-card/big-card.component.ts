24bd0ab1b1b37cd5c62240fbb828e34eedce3793
Sophia Portela <sophianobreportela@gmail.com>
2025-03-24 18:12:45 -0300

    components dynamics properties

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   imports: [],
   templateUrl: './big-card.component.html',
-  styleUrl: './big-card.component.sass'
+  styleUrl: './big-card.component.sass',
 })
 export class BigCardComponent {
+  @Input()
+  photoCover: string = '';
 
+  @Input()
+  cardTitle: string = '';
+
+  @Input()
+  cardDescription: string = '';
+
+  @Input()
+  cardLink: string = '';
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   imports: [],
   templateUrl: './small-card.component.html',
-  styleUrl: './small-card.component.sass'
+  styleUrl: './small-card.component.sass',
 })
 export class SmallCardComponent {
+  @Input()
+  photoCover: string = '';
 
+  @Input()
+  carTitle: string = '';
+
+  @Input()
+  cardLink: string = '';
 }
